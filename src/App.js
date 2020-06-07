518815d3bc36bc75f50dfe27e971d783b8dca079
solmii <rkdthfal4759@naver.com>
2020-06-07 23:28:26 +0900

    add react practice

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Subject title='WEB' sub='World Wide Web!' />
+        <Subject title='React' sub='For UI!' />
+        <TOC />
+        <Content title='HTML' desc='HTML is HyperText Markup Language.' />
+      </div>
+    );
+  }
+}
+
+class Subject extends Component {
+  render() {
+    return (
+      <header>
+        <h1>{this.props.title}</h1>
+        {this.props.sub}
+      </header>
+    );
+  }
+}
+
+class TOC extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li>
+            <a href='1.html'>HTML</a>
+          </li>
+          <li>
+            <a href='2.html'>CSS</a>
+          </li>
+          <li>
+            <a href='3.html'>JavaScript</a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+      <article>
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
+      </article>
+    );
+  }
+}
+
+export default App;
